f7684e88c2676862278566584b6710f641b2a69b
Kusum-1BM19CS077 <71306967+Kusum-1BM19CS077@users.noreply.github.com>
2020-12-08 12:07:23 +0530

    Update week11ep2.java

--- Week 11_extra programs/week11ep2.java
+++ Week 11_extra programs/week11ep2.java
@@ -38,10 +38,16 @@ class Number extends Thread
  {
  int randomInteger = random.nextInt(100);
  System.out.println("Random Integer generated : " + randomInteger);
+ if(randomInteger%2==0)
+ {
  Square s = new Square(randomInteger);
  s.start();
+ }
+ else
+ {
  Cube c = new Cube(randomInteger);
  c.start();
+ }
  try {
  Thread.sleep(1000);
 
