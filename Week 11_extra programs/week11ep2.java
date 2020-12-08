84d7f0c06ac0eba6dead1a28921698f76fa54e1f
Kusum-1BM19CS077 <71306967+Kusum-1BM19CS077@users.noreply.github.com>
2020-12-08 11:33:57 +0530

    Add files via upload

---
+++ Week 11_extra programs/week11ep2.java
@@ -0,0 +1,60 @@
+/*Develop a multithreaded Java program to create three threads. First thread generates
+random integer for every second and if the value is even, second thread computes the
+square of number and prints. If the value is odd, the third thread will print the value of
+cube of number.*/
+import java.util.Random;
+class Square extends Thread
+{
+ int x;
+ Square(int n)
+ {
+ x = n;
+ }
+ public void run()
+ {
+ int sqr = x * x;
+ System.out.println("Square of " + x + " = " + sqr );
+ }
+}
+class Cube extends Thread
+{
+ int x;
+ Cube(int n)
+ {
+ x = n;
+ }
+ public void run()
+ {
+ int cub = x * x * x;
+ System.out.println("Cube of " + x + " = " + cub );
+ }
+}
+class Number extends Thread
+{
+ public void run()
+ {
+ Random random = new Random();
+ for(int i =0; i<10; i++)
+ {
+ int randomInteger = random.nextInt(100);
+ System.out.println("Random Integer generated : " + randomInteger);
+ Square s = new Square(randomInteger);
+ s.start();
+ Cube c = new Cube(randomInteger);
+ c.start();
+ try {
+ Thread.sleep(1000);
+
+ } catch (InterruptedException ex) {
+ System.out.println(ex);
+}
+ }
+ }
+}
+class thread {
+ public static void main(String args[])
+ {
+ Number n = new Number();
+ n.start();
+ }
+}
